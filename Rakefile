8050cbbb7e3dba1a7d9787cf8a6bb4cb442c7d75
Amaia Castro <amaia@amaiac.net>
2017-06-20 17:31:19 +0200

    wip

--- Rakefile
+++ Rakefile
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
+require "bundler/gem_tasks"
+require "rspec/core/rake_task"
 require "decidim/dev/common_rake"
+
+load "lib/tasks/test_app.rake"
+
+RSpec::Core::RakeTask.new(:spec)
+
+task default: :spec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
 ENV["ENGINE_NAME"] = File.dirname(File.dirname(__FILE__)).split("/").last
+ENV["DECIDIM_ROOT_PATH"] = File.expand_path(".", Dir.pwd)
+ENV["DECIDIM_ENGINE_PATH"] = File.expand_path(".", Dir.pwd)
+
 require "decidim/dev/test/base_spec_helper"
